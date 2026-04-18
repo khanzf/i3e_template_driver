d312f5660bc0d30e2e0651c59e84407fbc2ed874
Farhan Khan <farhan@farhan.codes>
2026-04-17 21:18:56 -0400

    Adding sta_recv_mgmt.d dtrace script

--- dtrace/sta_recv_mgmt.d
+++ dtrace/sta_recv_mgmt.d
@@ -2,32 +2,37 @@
 
 #pragma D option quiet
 
-fbt::sta_recv_mgmt:entry
+string wlandev;
+
+dtrace:::BEGIN
 {
-	this->node = (struct ieee80211_node *)arg0;
-	this->vap = (struct ieee80211vap *)this->node->ni_vap;
-
- 	this->is_beacon_bad = this->vap->iv_stats.is_beacon_bad;
-
-	printf("Entry of sta_recv_mgmt\n");
-	printf("==============================\n");
-	printf("arg0: ieee80211_node *ni:      0x%p\n", arg0); 
-	printf("arg1: struct mbuf *m0:         0x%p\n", arg1); 
-	printf("arg2: int subtype:             0x%x\n", arg2); 
-	printf("arg3: ieee80211_rx_stats *rxs: 0x%02x\n", arg3);
-	printf("arg4: int rssi:                %d\n", arg4);
-	printf("arg5: int nf:                  %d\n", arg5);
-	printf("-------------------------------\n");
-	printf("Interface name: %s\n", stringof(this->vap->iv_ifp->if_xname));
-
-	printf("VAP state: %d\n", this->vap->iv_state);
-	printf("VAP flags: 0x%x\n", this->vap->iv_flags);
-	printf("vap->iv_stats.is_rx_beacon: %d\n", this->vap->iv_stats.is_rx_beacon);
-	printf("vap->iv_stats.is_rx_mgtdiscard: %d\n", this->vap->iv_stats.is_rx_mgtdiscard);
+	wlandev = $$1;
+        printf("Tracing started for interface: %s\n", wlandev);
 }
 
-fbt::sta_recv_mgmt:return
+fbt::sta_recv_mgmt:entry
 {
-	printf("Return of sta_recv_mgmt");
-	printf("==============================\n");
+        this->node = (struct ieee80211_node *)arg0;
+        this->vap = (struct ieee80211vap *)this->node->ni_vap;
+
+        this->is_beacon_bad = this->vap->iv_stats.is_beacon_bad;
+        this->ifname = stringof(this->vap->iv_ifp->if_xname);
+
+        if (this->ifname == wlandev) {
+                printf("Entry of sta_recv_mgmt\n");
+                printf("==============================\n");
+                printf("arg0: ieee80211_node *ni:      0x%p\n", arg0);
+                printf("arg1: struct mbuf *m0:         0x%p\n", arg1);
+                printf("arg2: int subtype:             0x%x\n", arg2);
+                printf("arg3: ieee80211_rx_stats *rxs: 0x%02x\n", arg3);
+                printf("arg4: int rssi:                %d\n", arg4);
+                printf("arg5: int nf:                  %d\n", arg5);
+                printf("-------------------------------\n");
+                printf("Interface name: %s\n", this->ifname);
+
+                printf("VAP state: %d\n", this->vap->iv_state);
+                printf("VAP flags: 0x%x\n", this->vap->iv_flags);
+                printf("vap->iv_stats.is_rx_beacon: %d\n", this->vap->iv_stats.is_rx_beacon);
+                printf("vap->iv_stats.is_rx_mgtdiscard: %d\n", this->vap->iv_stats.is_rx_mgtdiscard);
+        }
 }
